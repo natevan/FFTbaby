48d357c37c3a6956a93917e2b0e83e9fcc41ce67
Soto1999 <60240667+Soto1999@users.noreply.github.com>
2022-11-17 22:06:01 -0800

    Update MC.cu
    This one works

--- MC.cu
+++ MC.cu
@@ -30,6 +30,7 @@ void MC(double* answer_d){
     }
     bounding_box = box_height * box_width;
     
+    //count = 9;
     for (int j = 0; j < samples; j++)
     {
         x = (b-a)*curand_uniform(&state)+a;
@@ -37,14 +38,17 @@ void MC(double* answer_d){
         upper_limit = pow(x, 4);
         if (y <= upper_limit)
         {
-            count = 9;
-            printf("count++ %f\n", count);
+            count++;
+            //printf("count++ %d\n", count);
         }
     }
+    //printf("Bounding Box After%f\n", (double(count)/samples)*bounding_box);
+    answer_d[i] = (double(count)/samples)*bounding_box; 
+    //printf("%f", answer_d[i]);
     /*
     if(i == 0)
     {
-        printf("count %f samples %f box height %f  box width %f bounding box %f\n", count, samples, box_height, box_width, bounding_box);
+        printf("count %d samples %f box height %f  box width %f bounding box %f\n", count, samples, box_height, box_width, bounding_box);
     }
     answer_d[i] = ((double)count / samples) * bounding_box;
     */
@@ -55,6 +59,7 @@ int main()
     int size = sizeof(double)*samples;
     double *answer = (double*)malloc(size);
     double *answer_d;
+    double sum = 0;
     cudaMalloc((void**) &answer_d, size);
     dim3 block(1, 1);
     dim3 blockdim(1024, 1);
@@ -62,10 +67,10 @@ int main()
     cudaDeviceSynchronize();
     cudaMemcpy(answer, answer_d, size, cudaMemcpyDeviceToHost);
     for (int i = 0; i<samples; i++)
-    /*
     {
-        printf("%f\n", answer[i]);
+        sum += answer[i];
     }
-    */
+    double avg = sum/samples;
+    printf("Answer: %f\n", avg);
     return 0;
 }
